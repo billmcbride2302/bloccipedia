10612135e40f2bb90857518f566a1e47c334a625
Bill <bill@pcrsocial.com>
2017-10-03 22:04:26 -0400

    design changes

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -43,7 +43,7 @@ class WikiPolicy < ApplicationPolicy
       elsif user.role == 'premium'
         all_wikis = scope.all
         all_wikis.each do |wiki|
-          if !wiki.private? || wiki.user == user || wiki.users.include?(user)
+          if !wiki.private? || wiki.user == user 
             wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
           end
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'charges#new'
+  root to: 'welcome#index'
   devise_for :users
   resources :users do
     member do
